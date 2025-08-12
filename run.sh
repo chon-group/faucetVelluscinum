3ef7f7e022b29d5a47e7f2dcf5374d713c8f4fc1
Nilson Lazarin <nilsonmori@yahoo.com>
2025-08-12 18:28:07 -0300

    script install velluscinum

--- run.sh
+++ run.sh
@@ -1,3 +1,21 @@
 #! /bin/sh
+
+#! /bin/bash
+FRAMEWORK="/usr/bin/npm"
+VELLUSCINUM="/usr/bin/velluscinum"
+
+clear
+if [[ ! -f "$FRAMEWORK"  ]] || [[ ! -e "$VELLUSCINUM" ]]
+then
+    echo "Installing dependencies..."
+    sudo clear
+    echo "deb [trusted=yes] http://packages.chon.group/ chonos main" | sudo tee /etc/apt/sources.list.d/chonos.list
+    sudo apt update
+    sudo apt install npm velluscinum-cli -y
+    clear
+else
+    echo "The computer has Jason, SimulIDE and Velluscinum"
+fi
+
 npm install
 npm start
